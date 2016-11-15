11a28acd5927b9509f6c93d29d9165344c2e14bb
WiNiFiX <winifix@users.noreply.github.com>
2016-11-15 09:33:15 +0200

    // Build: 7.1.0.22996 x86

--- MemoryMagic/Offsets.cs
+++ MemoryMagic/Offsets.cs
@@ -4,11 +4,11 @@ namespace MemoryMagic
 {
     public static class Offsets
     {
-		// Build: 7.1.0.22908
-		public static IntPtr Framescript_ExecuteBuffer = new IntPtr(0x000A65F5);
-		public static IntPtr ClntObjMgrGetActivePlayerObj = new IntPtr(0x00081BCB);
-		public static IntPtr GameState = new IntPtr(0x00EAB6D9);
-		public static IntPtr FrameScript__GetLocalizedText = new IntPtr(0x002F9AF4);
-		public static IntPtr PlayerNameOffset = new IntPtr(0x00F8AF50);
+        // Build: 7.1.0.22996 x86
+        public static IntPtr Framescript_ExecuteBuffer = new IntPtr(0x000A6B7B);
+        public static IntPtr ClntObjMgrGetActivePlayerObj = new IntPtr(0x00081C7A);
+        public static IntPtr GameState = new IntPtr(0x00EAC715);
+        public static IntPtr FrameScript__GetLocalizedText = new IntPtr(0x002FA3B0);
+        public static IntPtr PlayerNameOffset = new IntPtr(0x00F8BF70);
     }
 }
